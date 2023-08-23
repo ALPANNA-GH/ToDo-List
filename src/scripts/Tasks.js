74c9b48d0e25bb1bf6ba7a930ed08aa746774c92
Ahmed Alpanna <alpanna@gmail.com>
2023-08-23 23:04:52 +0200

    all css js html file structur done

--- src/scripts/Tasks.js
+++ src/scripts/Tasks.js
@@ -1,27 +1,29 @@
-
-
 export class Tasks {
-  constructor() {
+  constructor () {
     this.taskList = [
       { index: 1, desc: 'task1', done: false },
       { index: 2, desc: 'task2', done: false },
-      { index: 3, desc: 'task3', done: false }
+      { index: 3, desc: 'task3', done: false },
+      { index: 4, desc: 'task4', done: false },
+      { index: 5, desc: 'task5', done: false },
+      { index: 6, desc: 'task7', done: true }
+
     ]
   }
 
-  addTask(task) {
+  // addTask(task) {
 
-  }
+  // }
 
-  removeTask(task) {
+  // removeTask(task) {
 
-  }
+  // }
 
-  editTask(task) {
+  // editTask(task) {
 
-  }
+  // }
 
-  reOrderTask(task) {
+  // reOrderTask(task) {
 
-  }
+  // }
 }
