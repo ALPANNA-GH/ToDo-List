352009f0a23088df813986b98c1168fff6e118bf
Ahmed Alpanna <alpanna@gmail.com>
2023-08-24 11:58:04 +0200

    addTask method done

--- src/scripts/Tasks.js
+++ src/scripts/Tasks.js
@@ -1,17 +1,24 @@
 export class Tasks {
   constructor () {
-    this.taskList = [
-      { index: 1, description: 'task1', completed: false },
-      { index: 2, description: 'task2', completed: false },
-      { index: 3, description: 'task3', completed: false },
-      { index: 4, description: 'task4', completed: false },
-      { index: 5, description: 'task5', completed: false },
-      { index: 6, description: 'task7', completed: true }
-    ]
+    this.getLocalStorage()
+    this.taskName = document.querySelector('#taskName')
+    // { index: 1, description: 'task1', completed: false },
+    // { index: 2, description: 'task2', completed: false },
+    // { index: 3, description: 'task3', completed: false },
+    // { index: 4, description: 'task4', completed: false },
+    // { index: 5, description: 'task5', completed: false },
+    // { index: 6, description: 'task7', completed: true }
   }
 
-  addTask (task) {
-    alert('task')
+  addTask () {
+    const taskMaxId = Math.max(...this.taskList.map((task) => task.id), 0)
+    const newTask = {
+      index: taskMaxId + 1,
+      description: this.taskName.textContent,
+      completed: false
+    }
+    this.taskList.push(newTask)
+    this.setLocalStorage()
   }
 
   removeTask (task) {
@@ -24,5 +31,13 @@ export class Tasks {
 
   reOrderTask (task) {
     alert('task')
+  }
+
+  getLocalStorage () {
+    this.taskList = JSON.parse(localStorage.getItem('taskList')) || []
+  }
+
+  setLocalStorage () {
+    localStorage.setItem('taskList', JSON.stringify(this.taskList))
   }
 }
